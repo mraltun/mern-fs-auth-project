a6e9258ed56f4270a20a92676b9f49ea28fe286d
Murat Altun <mraltun@gmail.com>
2022-08-07 18:25:53 +0300

    Added encryption to password fields in the backend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const jwt = require("jsonwebtoken");
 const User = require("./models/userModel");
+const bcrypt = require("bcryptjs");
 
 app.use(cors());
 app.use(express.json());
@@ -21,10 +22,11 @@ app.get("/", (req, res) => {
 
 app.post("/api/v1/register", async (req, res) => {
   try {
+    const newPassword = await bcrypt.hash(req.body.password, 10);
     const user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: newPassword,
     });
     res.json({ status: "ok" });
   } catch (err) {
@@ -35,10 +37,18 @@ app.post("/api/v1/register", async (req, res) => {
 app.post("/api/v1/login", async (req, res) => {
   const user = await User.findOne({
     email: req.body.email,
-    password: req.body.password,
   });
 
-  if (user) {
+  if (!user) {
+    return { status: "error", error: "Invalid login" };
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (isPasswordValid) {
     const token = jwt.sign(
       {
         name: user.name,
