dd4f1bca97b20c377f7e51e6cb02179dbf0db83d
Murat Altun <mraltun@gmail.com>
2022-08-07 16:46:48 +0300

    Token checks in the Dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const populateQuote = async () => {
+    const request = await fetch("http://localhost:8000/api/v1/quote", {
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    });
+
+    const data = request.json();
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      const user = jwt_decode(token);
+      if (!user) {
+        localStorage.removeItem("token");
+        navigate("/");
+      } else {
+        populateQuote();
+      }
+    }
+  }, []);
+
   return <div>Dashboard</div>;
 };
 
