6c9b68a7d806617fccdcc497683cb052dc2c1cd1
Murat Altun <mraltun@gmail.com>
2022-08-07 14:08:55 +0300

    Added routes for Login and Register pages in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Pages
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Hello</h1>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
