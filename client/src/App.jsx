97bdc1e121739f3d3aa7e4f3388e3cfd70a3a098
Murat Altun <mraltun@gmail.com>
2022-08-07 12:56:16 +0300

    Added register form in App

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async (e) => {};
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={registerUser}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type='text'
+          placeholder='Name'
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+          placeholder='Email'
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type='password'
+          placeholder='Password'
+        />
+        <button type='submit'>Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
 app.listen(8000, () => {
   console.log("Server running on 8000");
 });
