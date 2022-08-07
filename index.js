46423fa9321087048e1c38713b35b1b65d8b08c5
Murat Altun <mraltun@gmail.com>
2022-08-07 13:13:03 +0300

    Added route for register in backend

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
+});
+
+app.post("/api/v1/register", (req, res) => {
+  res.json({ status: "okay" });
 });
 
 app.listen(8000, () => {
